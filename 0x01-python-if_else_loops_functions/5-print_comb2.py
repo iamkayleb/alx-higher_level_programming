9520e7df40d0d024c3583779e162d8a70d8f780e
Caleb Ogbike <calebogbike@gmail.com>
2023-01-21 08:46:42 +0000

    printed the numbers on a straight line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print("0{},".format(i))
+        print("0{},".format(i), end="")
     else:
-        print("{},".format(i))
+        print("{},".format(i), end="")
