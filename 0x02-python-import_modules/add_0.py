a7a9a0651c3ae4a309661bb14c9a3fffe9d55162
Caleb Ogbike <calebogbike@gmail.com>
2023-01-24 04:19:02 -0800

    Update on first tasj

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, a+b))
-    return()
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
