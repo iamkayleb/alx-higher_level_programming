e9e558801933f42b81c79afd0ff6c1d781074244
Caleb Ogbike <calebogbike@gmail.com>
2023-01-24 18:25:21 -0800

    update on task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,11 @@
 import sys
 args = len(sys.argv)
 args2 = sys.argv
-#for i in args:
-if args == 1:
-    print("{} argument.".format(args))
-else:
-    print("{} arguments:".format(args))
+if args == 1 and args > 2:
+    print("{} arguments.".format(args - 1))
+#elif args > 2:
+#    print("{} arguments:".format(args - 1))
+elif args == 2:
+    print("{} argument:".format(args - 1))
 for i in range(1, args):
     print("{}: {}".format(i, args2[i]))
