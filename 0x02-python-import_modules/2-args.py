8fe3446a9dba4cb38a155dda6eb1e760b4f74744
Caleb Ogbike <calebogbike@gmail.com>
2023-01-24 18:29:12 -0800

    fixed zero issue

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 import sys
 args = len(sys.argv)
 args2 = sys.argv
-if args == 1 and args > 2:
+if args == 1:
     print("{} arguments.".format(args - 1))
-#elif args > 2:
-#    print("{} arguments:".format(args - 1))
+elif args > 2:
+    print("{} arguments:".format(args - 1))
 elif args == 2:
     print("{} argument:".format(args - 1))
 for i in range(1, args):
