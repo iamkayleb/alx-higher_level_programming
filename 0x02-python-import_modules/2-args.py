e8fe025ce703c5b3c8aae5f7934c0d4f2b38f132
Caleb Ogbike <calebogbike@gmail.com>
2023-01-24 18:39:34 -0800

    fixed execution error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-args = len(sys.argv)
-args2 = sys.argv
-if args == 1:
-    print("{} arguments.".format(args - 1))
-elif args > 2:
-    print("{} arguments:".format(args - 1))
-elif args == 2:
-    print("{} argument:".format(args - 1))
-for i in range(1, args):
-    print("{}: {}".format(i, args2[i]))
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv)
+    args2 = sys.argv
+    if args == 1:
+        print("{} arguments.".format(args - 1))
+    elif args > 2:
+        print("{} arguments:".format(args - 1))
+    elif args == 2:
+        print("{} argument:".format(args - 1))
+    for i in range(1, args):
+        print("{}: {}".format(i, args2[i]))
