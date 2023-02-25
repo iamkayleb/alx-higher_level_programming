d73f4fc07db14af5e86706a3290de7233231c40a
Caleb Ogbike <calebogbike@gmail.com>
2023-02-25 13:54:34 -0800

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for elm in my_list:
             print(elm, end="")
             x = x + 1
-        print(x, end="")
+        print("\n{}".format(x))
     except ValueError:
         pass
 safe_print_list()
