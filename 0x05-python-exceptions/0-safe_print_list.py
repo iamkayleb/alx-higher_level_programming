20671b26c59b7a8672c48376c89916d3c7ba13d6
Caleb Ogbike <calebogbike@gmail.com>
2023-02-25 12:30:17 -0800

    jdjddj

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     my_list = [1, 2, 3, 4,5,]
     try:
         for elm in my_list:
+            print(elm, end="")
             x = x + 1
-        print(x)
+        print(x, end="")
     except ValueError:
         pass
 safe_print_list()
