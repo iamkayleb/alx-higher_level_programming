e3bdb47d2b2acf74d9e836355efd71581174d728
Caleb Ogbike <calebogbike@gmail.com>
2023-03-05 08:06:47 -0800

    update 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,14 +15,8 @@ class Square:
 
         """
 
-        if size is int:
-            try:
-                pass
-            except TypeError:
-                print("size must be an integer")
+        if isinstance(size, int):
+            raise TypeError('size must be an integer')
         elif size < 0:
-            try:
-                pass
-            except ValueError:
-                print('size must be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = size
