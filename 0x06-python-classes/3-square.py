051cf819c111fefb4d9f9099b779e7bb93382ad3
Caleb Ogbike <calebogbike@gmail.com>
2023-03-05 08:34:01 -0800

    update 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Define a square"""
+
+
+class Square:
+    """a square class"""
+
+    def __init__(self, size=0):
+        """Initialises a square
+          Args:
+          self (int): size of square
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        "returns area of the square"
+        pass
