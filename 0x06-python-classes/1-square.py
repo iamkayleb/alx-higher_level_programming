b00be141c25f3e8d9b267d32109295d2266bc4fe
Caleb Ogbike <calebogbike@gmail.com>
2023-03-05 04:21:20 -0800

    update 4

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+""""Defines a new square"""
+
 
 class Square:
     " a square class"
+
     def __init__(self, size):
-        """Initialises a new square
+        """Initialises a new square.
+
         Args:
             size (int): The size of the new square
-
         """
         self.__size = size
