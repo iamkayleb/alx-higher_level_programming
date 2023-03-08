9c51e9fcde67a08e498114a6e1f5f5dd9b85c4c7
Caleb Ogbike <calebogbike@gmail.com>
2023-03-08 09:11:06 -0800

    added docstrings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+"""
+This is the addition module
+
+This module supplies one function that adds two integers together
+
+"""
 def add_integer(a, b=98):
+    """
+    Function adds two integers together
+    
+    Args:
+    a (int); first integer
+    b (int): second integer
+
+    FLoats are converted to integers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    elif type(a, b) is float:
+        int(a, b)
     return (a + b)
