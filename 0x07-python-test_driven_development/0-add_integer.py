44e8e4455934cbc5c43981871c4536d6c739d727
Caleb Ogbike <calebogbike@gmail.com>
2023-03-08 09:43:00 -0800

    uodate 6

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,8 +17,7 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    elif type(sum(a, b)) is float:
-        return (sum(a, b))
-    return (sum(a, b))
+    
+    return (int(a) + int(b))
