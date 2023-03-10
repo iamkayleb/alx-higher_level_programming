a46602cbf84928af2616225918e5b08c5b4e84c0
Caleb Ogbike <calebogbike@gmail.com>
2023-03-10 03:17:44 -0800

    update on 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,12 +9,13 @@ class Square():
         Args:
         size (int): the sixe of the square
         """
-       if not isinstance(size, int):
+        if not isinstance(size, int):
            raise TypeError("size must be an integer")
-       if size < 0:
+        if size < 0:
            raise ValueError("size must be >- 0")
 
-       self.__size = size
+        self.__size = size
+
     def area(self):
         """Methos returns the area of the square
         
